be049a9f5eb91de8e15620791c318fdee9905a0e
alienzj <alienchuj@gmail.com>
2020-03-23 00:07:12 +0800

    update count.c, count line, word, char number

--- learn_c/src/count.c
+++ learn_c/src/count.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main()
 {
-    int c, nl;
+    int c, nl, nw, nc, state;
 
-    nl = 0;
+    state = OUT;
+    nl = nw = nc = 0;
 
-    while ((c = getchar()) != EOF)
+    while ((c = getchar()) != EOF) {
+        ++nc;
         if (c == '\n')
             ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
 
     printf("\n%d\n", nl);
 }
