1461454e633dbefbf533ef91ab5bad7d26ad57ca
alienzj <alienchuj@gmail.com>
2020-03-26 20:56:37 +0800

    using learn_c

--- learn_c/src/hello.c
+++ learn_c/src/hello.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    char a = 1;
     printf("hello, world\n");
     return 0;
 }
--- learn_c/src/io.c
+++ learn_c/src/io.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+    // ex1
     FILE * fp = fopen("io.c", "r");
     if (!fp) {
         perror("File opening failed");
@@ -19,5 +20,23 @@ int main()
     else if (feof(fp))
         puts("End of file reached sucessfully");
     fclose(fp);
+
+    // ex2
+    FILE* tmpf = tmpfile();
+    fputs("Hello, World!\n", tmpf);
+    fputs("Welcome to the bioinformatics community\n", tmpf);
+    fputs("Alan Turing\n", tmpf);
+    fputs("John von Neumann\n", tmpf);
+    fputs("Alonzo Chruch\n", tmpf);
+
+    rewind(tmpf);
+
+    char buf[8];
+    while (fgets(buf, sizeof buf, tmpf) != NULL)
+        printf("\"%s\"\n", buf);
+
+    if (feof(tmpf))
+        puts("End of file reached");
+
     return 0;
 }
