fd58aa266c562acc31bfb53fb02bf877ebb95cbd
alienzj <alienchuj@gmail.com>
2020-03-26 21:17:16 +0800

    simplfy

--- learn_c/src/io.c
+++ learn_c/src/io.c
@@ -1,42 +1,74 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <zlib.h>
 
-int main()
-{
-    // ex1
-    FILE * fp = fopen("io.c", "r");
-    if (!fp) {
-        perror("File opening failed");
-        return EXIT_FAILURE;
+#define LENS 1000000
+
+int count_from_stdin() {
+  int c;
+  long nc = 0;
+
+  while ((c = getchar()) != EOF) {
+    // EOF: end of file
+    putchar(c);
+    ++nc;
+  }
+
+  printf("\n%ld\n", nc);
+
+  return 0;
+}
+
+unsigned long file_size(char *file_name) {
+  FILE *file = fopen(file_name, "rb");
+  fseek(file, 0, SEEK_END);
+  unsigned long size = ftell(file);
+  fclose(file);
+  return size;
+}
+
+size_t fq(const char *fname) {
+  fprintf(stderr, "\t-> using fgets\n");
+  FILE *fp = fopen(fname, "r");
+  size_t n_lines = 0;
+  char buffer[LENS];
+  while (NULL != fgets(buffer, LENS, fp))
+    n_lines++;
+
+  fprintf(stderr, "%lu\n", n_lines);
+  return n_lines;
+}
+
+int print_gzfile(const char *file_name) {
+  gzFile file_in = gzopen(file_name, "rb");
+
+  if (!file_in) {
+    fprintf(stderr, "gzopen of '%s' failed: %s.\n", file_name, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    int err;
+    int bytes_read;
+    unsigned char buffer[1000];
+    bytes_read = gzread(file_in, buffer, 999);
+    buffer[bytes_read] = '\0';
+    printf("%s", buffer);
+    if (bytes_read < 999) {
+      if (gzeof(file_in)) {
+        break;
+      } else {
+        const char *error_string;
+        error_string = gzerror(file_in, &err);
+        if (err) {
+          fprintf(stderr, "Error: %s.\n", error_string);
+          exit(EXIT_FAILURE);
+        }
+      }
     }
-
-    int c;
-    while((c = fgetc(fp)) != EOF) {
-        putchar(c);
-    }
-
-    if (ferror(fp))
-        puts("I/O error when reading");
-    else if (feof(fp))
-        puts("End of file reached sucessfully");
-    fclose(fp);
-
-    // ex2
-    FILE* tmpf = tmpfile();
-    fputs("Hello, World!\n", tmpf);
-    fputs("Welcome to the bioinformatics community\n", tmpf);
-    fputs("Alan Turing\n", tmpf);
-    fputs("John von Neumann\n", tmpf);
-    fputs("Alonzo Chruch\n", tmpf);
-
-    rewind(tmpf);
-
-    char buf[8];
-    while (fgets(buf, sizeof buf, tmpf) != NULL)
-        printf("\"%s\"\n", buf);
-
-    if (feof(tmpf))
-        puts("End of file reached");
-
-    return 0;
+  }
+  gzclose(file_in);
+  return 0;
 }
