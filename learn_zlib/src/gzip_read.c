b4662320126551aff6fe37e2790a9c56c3e69605
alienzj <alienchuj@gmail.com>
2020-03-26 10:21:58 +0800

    add gzip_read.c

---
+++ learn_zlib/src/gzip_read.c
@@ -0,0 +1,42 @@
+#include "zlib.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    const char *file_name = argv[1];
+    printf("%s\n", file_name);
+    gzFile file_in = gzopen(file_name, "rb");
+
+    if (!file_in) {
+        fprintf(stderr, "gzopen of '%s' failed: %s.\n", file_name,
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        int err;
+        int bytes_read;
+        unsigned char buffer[1000];
+        bytes_read = gzread(file_in, buffer, 999);
+        buffer[bytes_read] = '\0';
+        printf("%s", buffer);
+        if (bytes_read < 999) {
+            if (gzeof(file_in)) {
+                break;
+            }
+            else {
+                const char* error_string;
+                error_string = gzerror(file_in, &err);
+                if (err) {
+                    fprintf(stderr, "Error: %s.\n", error_string);
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+    gzclose(file_in);
+    return 0;
+}
