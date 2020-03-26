c0b530e3b369332e7170c90530dda08caabb1ee4
alienzj <alienchuj@gmail.com>
2020-03-27 05:50:02 +0800

    restructure

--- learn_c/io.c
+++ learn_c/io.c
@@ -9,15 +9,12 @@
 int count_from_stdin() {
   int c;
   long nc = 0;
-
   while ((c = getchar()) != EOF) {
     // EOF: end of file
     putchar(c);
     ++nc;
   }
-
   printf("\n%ld\n", nc);
-
   return 0;
 }
 
@@ -36,19 +33,26 @@ size_t fq(const char *fname) {
   char buffer[LENS];
   while (NULL != fgets(buffer, LENS, fp))
     n_lines++;
-
   fprintf(stderr, "%lu\n", n_lines);
+  return n_lines;
+}
+
+size_t iz(const char *fname) {
+  fprintf(stderr, "\t-> using zlib\n");
+  gzFile fp = gzopen(fname, "r");
+  size_t n_lines = 0;
+  char buffer[LENS];
+  while (0 != gzgets(fp, buffer, LENS))
+    n_lines++;
   return n_lines;
 }
 
 int print_gzfile(const char *file_name) {
   gzFile file_in = gzopen(file_name, "rb");
-
   if (!file_in) {
     fprintf(stderr, "gzopen of '%s' failed: %s.\n", file_name, strerror(errno));
     exit(EXIT_FAILURE);
   }
-
   while (1) {
     int err;
     int bytes_read;
